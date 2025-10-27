0eb7481ea49c422a95a82835231413265360664e
Jesper Alf Dam <github@jalf.dk>
2025-10-27 23:31:22 +0100

    Handle null when reading from localStorage

--- src/local-storage-sync.ts
+++ src/local-storage-sync.ts
@@ -2,6 +2,6 @@ export function write(state: string) {
   sessionStorage.setItem("rand-o-tron.state", state);
 }
 
-export function read(): string | null {
-  return sessionStorage.getItem("rand-o-tron.state");
+export function read(): string {
+  return sessionStorage.getItem("rand-o-tron.state") ?? "{}";
 }
